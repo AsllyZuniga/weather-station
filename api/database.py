0e30d8f6fac46ddf8cadcae1d46ca6a2caf4b7ae
Aslly Zuñiga <asllyz2610@gmail.com>
2024-10-21 21:43:17 -0500

    Modified crud and database

--- api/database.py
+++ api/database.py
@@ -15,12 +15,27 @@ cur = con.cursor()
 
 #Users model
 users_model = '''
-    CREATE TABLE IF NOT EXISTS user (
+    CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY,
         username TEXT NOT NULL,
         email TEXT NOT NULL,
         password TEXT NOT NULL,
-        role TEXT NOT NULL,
+        role INTEGER NOT NULL DEFAULT 1,
+        status BOOLEAN DEFAULT TRUE,
+        created_at TIMESTAMP DEFAULT (datetime('now','localtime')),
+        update_at TIMESTAMP DEFAULT (datetime('now','localtime')),
+        deleted_at NULL
+    )
+'''
+#Users model
+sensors_model = '''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        model TEXT NOT NULL,
+        description TEXT NOT NULL,
+        url_datasheet TEXT NULL,
+        url_image TEXT NULL,
         status BOOLEAN DEFAULT true,
         created_at TIMESTAMP DEFAULT (datetime('now','localtime')),
         update_at TIMESTAMP DEFAULT (datetime('now','localtime')),
@@ -29,6 +44,7 @@ users_model = '''
 '''
 #Execute query
 cur.execute(users_model)
+cur.execute(sensors_model)
 
 ##Close connection
-con.close()
+##con.close()
